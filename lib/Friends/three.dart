c3ce68f1521b278180d377690d87819c87af2af0
Lolan <muhd.afsxl@gmail.com>
2024-04-29 23:36:16 +0530

    done

--- lib/Friends/three.dart
+++ lib/Friends/three.dart
@@ -25,7 +25,6 @@ class _ThreeState extends State<Three> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
-        
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
